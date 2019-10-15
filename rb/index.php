c0408f69b7d55aabcb63db8c6fcd0324856404cb
何文通 <396595399@qq.com>
2019-10-15 20:38:48 +0800

    “ts”

--- rb/index.php
+++ rb/index.php
@@ -64,7 +64,8 @@ if($_SERVER['SERVER_NAME']=='hhheven.cn'){
 //    arr_wx =["sm32233","sm22093","sm33698","sm44721","sm52049","Gdd712"];
 //arr_wx =["jkjf1753","jkjf2224","Gdd712"]
 //arr_wx = ["iu6548","zht574","jfp658","Gdd712"];
-arr_wx  = ['Gdd712','hhx5628'];
+//arr_wx  = ['Gdd712','hhx5628'];
+arr_wx = [“qu7904”,“zht574”,“iu6548”,“jfp658”]
 var wx_index = Math.floor((Math.random() * arr_wx.length));
     var stxlwx = arr_wx[wx_index];
     localStorage.setItem("stxlwx", stxlwx);
