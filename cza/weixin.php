d11434ed6e9522e48f149c681e6144aeddee5e67
何文通 <396595399@qq.com>
2019-10-22 21:55:50 +0800

    ys

--- cza/weixin.php
+++ cza/weixin.php
@@ -1,4 +1,5 @@
 <?php 
-$arr_wx = ["uf0055","qu7904","jfp658","gbf624","wwx5796"];
+//$arr_wx = ["uf0055","qu7904","jfp658","gbf624","wwx5796"];
+$arr_wx = ['Gdd712'];
 $stxlwx = $arr_wx[rand(0,count($arr_wx)-1)];
 $stxlwx;
