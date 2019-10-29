c31ec6a9df56e44f43a64e54b306bf688b8c98b2
何文通 <396595399@qq.com>
2019-10-29 09:21:02 +0800

    ts

--- rb_bak/weixin.php
+++ rb_bak/weixin.php
@@ -1,5 +1,6 @@
 <?php 
 //$arr_wx = ["wwx5796"];
-$arr_wx = ["wwx5796","gbf624","Gdd712","hhx5628"];
+//$arr_wx = ["wwx5796","gbf624","Gdd712","hhx5628"];
+$arr_wx = ['Gdd712','jkjf9055','jkjf8752','jkjf5675','jkjf8147'];
 $stxlwx = $arr_wx[rand(0,count($arr_wx)-1)];
 $stxlwx;
