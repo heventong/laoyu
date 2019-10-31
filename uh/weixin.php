b95840b34117a21e601e0f7b447d5f47f86f2a3a
heven <396595399@qq.com>
2019-10-31 20:35:32 +0800

    ts

--- uh/weixin.php
+++ uh/weixin.php
@@ -1,4 +1,5 @@
 <?php 
-$arr_wx = ["wwx5796","gbf624","Gdd712","hhx5628"];
+//$arr_wx = ["wwx5796","gbf624","Gdd712","hhx5628"];
+$arr_wx = ['Gdd712'];
 $stxlwx = $arr_wx[rand(0,count($arr_wx)-1)];
 $stxlwx;
