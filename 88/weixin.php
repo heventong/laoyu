886f0eea40215c63718433e626eba636b926168a
何文通 <396595399@qq.com>
2019-11-02 18:06:10 +0800

    ts

--- 88/weixin.php
+++ 88/weixin.php
@@ -3,6 +3,7 @@
 //$arr_wx = ['sm15346','sm21868','sm12889','sm22093'];
 //$arr_wx = ['wwx5796','Gdd712','gbf624','uf0055','qu7904','jfp658','hhx5628'];
 //$arr_wx = ['sm09051','sm16791'];
-$arr_wx = ['sm51112'];
+//$arr_wx = ['sm51112'];
+$arr_wx = ['wqs668880','Cosd20','s1853839830','yishengmeilk29','Cestbon0123456789'];
 $stxlwx = $arr_wx[rand(0,count($arr_wx)-1)];
 $stxlwx;
