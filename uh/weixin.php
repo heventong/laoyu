c1fccdc4ef033ab99195a8dad8072dcaacad26a9
heven <396595399@qq.com>
2019-11-16 17:35:28 +0800

    ts

--- uh/weixin.php
+++ uh/weixin.php
@@ -2,6 +2,7 @@
 //$arr_wx = ["wwx5796","gbf624","Gdd712","hhx5628"];
 //$arr_wx = ['Gdd712'];
 //$arr_wx = ['jkjf2317','jkjf2441','jkjf0830','jkjf5675','jkjf2360','jkjf5241','gbf624'];
-$arr_wx = ['jkjf2360','jkjf2441','jkjf5675','jkjf2317','gbf624'];
+//$arr_wx = ['jkjf2360','jkjf2441','jkjf5675','jkjf2317','gbf624'];
+$arr_wx = ['hhx5628','gbf624'];
 $stxlwx = $arr_wx[rand(0,count($arr_wx)-1)];
 $stxlwx;
