e276ff4c2cfdd1d7716ea55659a6f22aedd93f83
何文通 <396595399@qq.com>
2019-11-22 20:10:57 +0800

    ts

--- jf/weixin.php
+++ jf/weixin.php
@@ -3,6 +3,6 @@
 //$arr_wx = ['Gdd712'];
 //$arr_wx = ['jkjf2317','jkjf2441','jkjf0830','jkjf5675','jkjf2360','jkjf5241','gbf624'];
 //$arr_wx = ['jkjf2360','jkjf2441','jkjf5675','jkjf2317','gbf624'];
-$arr_wx = ['jy992046'];
+$arr_wx = ['bxt839','jy992046'];
 $stxlwx = $arr_wx[rand(0,count($arr_wx)-1)];
 $stxlwx;
