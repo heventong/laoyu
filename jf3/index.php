1defe27aa414e5134df36556b6069b4101fe85d3
hewentong <396595399@qq.com>
2019-12-07 16:49:08 +0800

    ts

--- jf3/index.php
+++ jf3/index.php
@@ -1,6 +1,5 @@
 <?php 
 error_reporting(0);
-
  require_once(__DIR__.'/../vendor/autoload.php');
  $redis = new Predis\Client([
     'scheme' => 'tcp',
@@ -16,11 +15,13 @@ $redis->auth("tong123");
  }else{
     $redis->set($dirname."_index",$weixin_index+1);
  }
+//  var_dump($weixins,$weixin_index);exit;
  $stxlwx = $weixins[$weixin_index];
- $module_view = $redis -> get ($dirname."_view");
- if(!$module_view){
+ $dirview = $module_view = $redis -> get ($dirname."_view");
+ if($module_view=='1'){
      $module_view='shenhe.php';
- }else{
-    $module_view = 'ad.php';
- }
+ }else
+{
+    $module_view = $module_view.'.php';
+}
 include($module_view); 
