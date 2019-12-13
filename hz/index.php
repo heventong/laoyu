79f8cc85650afaf5cdbbab2685a9b3ec05b6f60a
何文通 <396595399@qq.com>
2019-12-13 11:54:13 +0800

    ts

--- hz/index.php
+++ hz/index.php
@@ -1,16 +1,9 @@
 <?php 
 error_reporting(0);
-// echo json_encode(["LDT168668","123","456","789"]);
-//  $redis = new Redis();
-//  $redis->connect("118.24.150.195","6379");
-//  $redis->auth("tong123");
  require_once(__DIR__.'/../vendor/autoload.php');
- $redis = new Predis\Client([
-    'scheme' => 'tcp',
-    'host'   => '118.24.150.195',
-    'port'   => 6379,
-]);
-$redis->auth("tong123");
+$redis_conf = include ("../redis.conf");
+ $redis = new Predis\Client($redis_conf);
+$redis->auth($redis_conf['auth']);
  $dirname = end(explode('/',dirname(__FILE__)));
  $weixins = json_decode($redis->get($dirname),true);
  $weixin_index = $redis->get($dirname."_index");
