0cd6b2e3a862225f04203c4ca81992b936ea70ff
何文通 <396595399@qq.com>
2019-12-16 14:17:56 +0800

    ts

---
+++ jf2/ad4/static/js/tz-loop.js
@@ -0,0 +1,14 @@
+
+var wxn = '秋艳';
+var ztfb='2019-12-15';var stxlwx='';
+
+//var test= ./image/.jpg;
+var mess3 = '';
+day = new Date();
+hr = day.getHours();
+//m = day.getMinutes();
+m =day.getSeconds();
+
+
+
+
