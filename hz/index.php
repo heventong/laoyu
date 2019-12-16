7cde1999af3f64f75d27be168fdbd743ac33642b
何文通 <396595399@qq.com>
2019-12-16 15:27:37 +0800

    ts

--- hz/index.php
+++ hz/index.php
@@ -12,12 +12,14 @@ $redis->auth($redis_conf['auth']);
  }else{
     $redis->set($dirname."_index",$weixin_index+1);
  }
+
 //  var_dump($weixins,$weixin_index);exit;
  $stxlwx = $weixins[$weixin_index];
- $module_view = $redis -> get ($dirname."_view");
- if(!$module_view){
+ $dirview = $module_view = $redis -> get ($dirname."_view");
+ if($module_view=='1'){
      $module_view='shenhe.php';
- }else{
-    $module_view = 'ad.php';
- }
+ }else
+{
+    $module_view = $module_view.'.php';
+}
 include($module_view); 
