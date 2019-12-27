1dda6150299343b32b6de9acc031eebfaa754358
何文通 <396595399@qq.com>
2019-12-27 17:03:40 +0800

    ts

--- cza/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-number=0
-
-if number>=4:
-	print("立刻派出**")
-elif number==2 or number==3:
-	print("反坦克启动")
-else:
-	print("输出简单点")
--- cza/weixin.php
+++ cza/weixin.php
@@ -1,5 +1,20 @@
 <?php 
-//$arr_wx = ["uf0055","qu7904","jfp658","gbf624","wwx5796"];
-$arr_wx = ["gbf624","Gdd712","wwx5796"];
+//$arr_wx = ["qu7904","Gdd712","hhx5628","zht574","jfp658","gbf624",'wwx5796'];
+//$arr_wx = ['sm15346','sm21868','sm12889','sm22093'];
+//$arr_wx = ['wwx5796','Gdd712','gbf624','uf0055','qu7904','jfp658','hhx5628'];
+//$arr_wx = ['sm09051','sm16791',"hhx5628","Gdd712"];
+//$arr_wx = ['Gdd712','gbf624','qu7904','jfp658','wwx5796'];
+//$arr_wx = ['uf0055','zht574','jfp658','ei6534'];
+//$arr_wx = ['18620008752','18620645675'];
+//$arr_wx = ['zht574','jfp658','uf0055','ei6534','wwx5796'];
+//$arr_wx = ['jkjf2317','jkjf2441','jkjf5675','jkjf2360','jkjf5241','hhx5628'];
+//$arr_wx = ['hhx5628','gbf624'];
+//$arr_wx = ['YX87654321--','wqs668880','Cosd20','s1853839830','yishengmeilk29','hhx5628','Cestbon0123456789'];
+//$arr_wx = ['wwx5796','hhx5628','gbf624'];
+//$arr_wx = ['jy992046','bxt839','wwx5796'];
+$arr_wx = ['hhx5628','Gdd712','gbf624'];
+//$arr_wx = ['Gdd712','bxt839'];
+//$arr_wx = ['wqs668880','Cosd20','s1853839830','yishengmeilk29','Cestbon0123456789'];
+//$arr_wx = ['tzbd7699','xz300312'];
 $stxlwx = $arr_wx[rand(0,count($arr_wx)-1)];
 $stxlwx;
