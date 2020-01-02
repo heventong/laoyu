36c6a3f90c8afa1ec8dfdc58f8b73b53c4c9260f
何文通 <396595399@qq.com>
2020-01-02 14:38:39 +0800

    ts

--- lzy/index.php
+++ lzy/index.php
@@ -1,9 +1,9 @@
 <?php 
 error_reporting(0);
  require_once(__DIR__.'/../vendor/autoload.php');
-$redis_conf = include ("../redis.conf");
+ $redis_conf = include ("../redis.conf");
  $redis = new Predis\Client($redis_conf);
-$redis->auth($redis_conf['auth']);
+ $redis->auth($redis_conf['auth']);
  $dirname = end(explode('/',dirname(__FILE__)));
  $weixins = json_decode($redis->get($dirname),true);
  $weixin_index = $redis->get($dirname."_index");
@@ -14,10 +14,11 @@ $redis->auth($redis_conf['auth']);
  }
 //  var_dump($weixins,$weixin_index);exit;
  $stxlwx = $weixins[$weixin_index];
- $module_view = $redis -> get ($dirname."_view");
- if(!$module_view){
+ $dirview = $module_view = $redis -> get ($dirname."_view");
+ if($module_view=='1'){
      $module_view='shenhe.php';
- }else{
-    $module_view = 'ad.php';
- }
+ }else
+{
+    $module_view = $module_view.'.php';
+}
 include($module_view); 
---
+++ lzy_bak0102/shenhe.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+<img style="width:100%" src="./danshen/index_files/1.png"/>
+<img style="width:100%" src="./danshen/index_files/2.png"/>
+<h3>广州丹升科技有限公司</h3>
+<div style="display: inline-block; position: absolute;"><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44010602000446" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px;color:#d4d4d4;">晋ICP备17005820号-1</p></a></div>
+
+
