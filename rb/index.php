55318817d0cefcebc17a26b25e457e34dbadf619
何文通 <396595399@qq.com>
2020-02-15 00:30:44 +0800

    添加时间判断

--- rb/index.php
+++ rb/index.php
@@ -12,7 +12,7 @@ error_reporting(0);
  }else{
     $redis->set($dirname."_index",$weixin_index+1);
  }
-$am_time = date("Y-m-d 00:00:00");
+$am_time = date("Y-m-d 09:00:00");
 $pm_time = date("Y-m-d 19:00:00");
 $cur_time = date("Y-m-d H:i:s");
 //  var_dump($weixins,$weixin_index);exit;
