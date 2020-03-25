8a88f5511f81e09ae2a35b4227890fb98e91fd21
何文通 <396595399@qq.com>
2020-03-25 15:08:40 +0800

    ts

---
+++ aqy2/shenhe3.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<!-- saved from url=(0024)http://xm1.neuedurx.com/ -->
+<html style="font-size: 200px;" lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	
+	
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>瘦身训练营-H站</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<link rel="stylesheet" href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/swiper.css">
+	<link rel="stylesheet" href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/common.css">
+    <script src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/hm.js.js.下载"></script>
+	<script type="text/javascript" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jquery_002.js.js.下载"></script>
+    <script type="text/javascript" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/swiper.js.js.下载"></script>
+    <script src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jquery.js.js.下载"></script>
+	
+    <script type="text/javascript" charset="utf-8">
+		$(function() {
+			$("img.lazyload").lazyload({
+				effect: "fadeIn",
+				threshold: 100
+			});
+		});
+	</script>
+	
+<script type="text/javascript" charset="utf-8" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/cdcheck.js.下载"></script><script type="text/javascript" charset="utf-8" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/JS_Float.js.下载"></script><script type="text/javascript" charset="utf-8" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/JS_Float.aspx"></script></head>
+
+
+
+<body>
+  <div class="common_main common_ln_640">
+  	<!--头部开始-->
+  	<!--页面采用rem布局-->
+
+<div class="common_top">
+	<div class="common_top_div common_ln_640 relative">
+	
+  		<div class="common_top_left">
+  			<!--<div class="common_top_left_dh">导航&gt;</div>-->
+			<!--<div>导航&gt;</div>-->
+  		</div>
+	<!--电话-->
+  	<a href="tel:4008946898" class="tingdiandianhua">
+	<div class="common_top_right">
+	<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/common_top_right.png" class="img pulse"></div>
+	</a>
+  		<div class="common_nav">
+  			
+	  		<div class="common_nav_1">首页</div>
+	  		
+	  		<div class="common_nav_1">减肥训练营</div>
+	  	
+	  		<div class="common_nav_1">减肥冬令营</div>
+	  		
+	  <div class="common_nav_1">减肥夏令营</div>
+	  		
+	  		<div class="common_nav_1">成功故事</div>
+	  		
+	  		<div class="common_nav_1">电视报道</div>
+	  		
+	  		<div class="common_nav_1">关于我们</div>
+	  		
+	  		
+	  	</div>
+	</div>
+</div>
+  	<!--头部结束-->
+	
+	
+	
+	
+  	<div class="common_div jfxly_div">
+  		<div class="common_ln jfxly_ln">
+  			<div class="jfxly_ln_2_1">
+  				
+  				<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxly_ln_2_1.jpg" class="img">
+  				
+  			</div>
+  			<div class="jfxly_ln_2_2">
+  				<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxly_ln_2_2.jpg" class="title_img">
+  				<div class="jfxly_ln_2_2_1">
+  					<div class="index_ln_3">
+
+		  			</div>
+  				</div>
+  			</div>
+			
+			
+  			<div class="jfxly_ln_2_3">
+  				<div class="jfxly_ln_2_3_1">
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxly_ln_2_3_1.jpg" style="display: inline;">
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxly_ln_2_3_2.jpg" style="display: inline;">
+  				</div>
+  				<!--<div class="jfxialy_index_2_ln_bottom">
+  					<div class="jfxialy_index_2_ln_bottom_div">
+  						<div class="title">限时优惠过期即涨</div>
+  						<div class="button chen" id="jfxly_swt_5"><a href="javascript:void(0)" onclick="openZoosUrl('chatwin','&amp;e=%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425_%25u6DF1%25u5733%25u51CF%25u80A5%25u8FBE%25u4EBA%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425-%25u70B9%25u51FB%25u54A8%25u8BE2');return false;" title="客服人员在线,欢迎点击咨询" target="_self">点击咨询</a></div>
+  					</div>-->
+  					<div class="yy"></div>
+  				</div>
+  			</div>
+			
+  			<div class="jfxly_ln_2_4">
+  				<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxly_ln_2_4_1.jpg" class="img">
+  				<div class="jfxialy_index_2_ln_top">
+  					<div class="tiwen chen">实力加身效果说话</div>
+  				
+  					<div class="yy"></div>
+  				</div>
+  				<div class="jfxly_ln_2_4_1">
+  					<div class="swiper-container swiper-cggs swiper-container-horizontal">
+					    <div class="swiper-wrapper" style="transition-duration: 0ms; transform: translate3d(-1280px, 0px, 0px);"><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next" data-swiper-slide-index="2" style="width: 640px;">
+					        <img class="swiper-lazy img button_img swiper-lazy-loaded" data-splj="?auto=0&amp;vid=b03494xwcya" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1-1p11511114y29.jpg">
+					      </div>
+					      <div class="swiper-slide swiper-slide-prev" data-swiper-slide-index="0" style="width: 640px;">
+					        <img class="swiper-lazy img button_img swiper-lazy-loaded" data-splj="?vid=c0514meyuy8&amp;tiny=0&amp;auto=0" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1-1p11511454d43.jpg">
+					      </div>
+<div class="swiper-slide swiper-slide-active" data-swiper-slide-index="1" style="width: 640px;">
+					        <img class="swiper-lazy img button_img swiper-lazy-loaded" data-splj="?auto=0&amp;vid=u0539hxagrc" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1-1p115111301m1.jpg">
+					      </div>
+<div class="swiper-slide swiper-slide-next" data-swiper-slide-index="2" style="width: 640px;">
+					        <img class="swiper-lazy img button_img swiper-lazy-loaded" data-splj="?auto=0&amp;vid=b03494xwcya" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1-1p11511114y29.jpg">
+					      </div>
+
+					    <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev" data-swiper-slide-index="0" style="width: 640px;">
+					        <img class="swiper-lazy img button_img swiper-lazy-loaded" data-splj="?vid=c0514meyuy8&amp;tiny=0&amp;auto=0" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1-1p11511454d43.jpg">
+					      </div></div>
+					    <div class="swiper-pagination swiper-pagination-white swiper-pagination-clickable swiper-pagination-bullets"><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span><span class="swiper-pagination-bullet"></span></div>
+					</div>
+					<div class="jfxialy_index_2_ln_bottom">
+	  					<div class="jfxialy_index_2_ln_bottom_div">
+	  						<div class="title">想减多少斤？能减多少斤？</div>
+	  						
+	  					</div>
+	  					<div class="yy"></div>
+	  				</div>
+  				</div>
+  			</div>
+			
+			
+  			<div class="jfxly_ln_2_6">
+  				<div class="jfxialy_index_2_ln_top">
+  					<div class="tiwen chen">8项管理安心高效</div>
+  					<div class="title">科学健康管理  高效保障</div>
+  					<div class="yy"></div>
+  				</div>
+  				<div class="jfxly_ln_2_6_1">
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxly_ln_2_6_1.jpg" style="display: inline;"> 
+  				</div>
+  				<div class="jfxialy_index_2_ln_bottom">
+  					<div class="jfxialy_index_2_ln_bottom_div">
+  						<div class="title">开营时间，周期，价格</div>
+  						
+  					</div>
+  					<div class="yy"></div>
+  				</div>
+  			</div>
+			
+			
+  			<div class="jfxialy_index_2_ln_6"><!--这里当做6-->
+  				<div class="jfxialy_index_2_ln_top">
+  					<div class="tiwen chen">高端营地度假体验？</div>
+  					<div class="title">规模够大 星级体验</div>
+  					<div class="yy"></div>
+  				</div>
+  				<div class="jfxialy_index_2_ln_6_1">
+  					
+  					<!--<a href="https://wap.jianfeidaren.com/beijing/">-->
+  						<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_bj.jpg" style="display: block;">
+  					
+  					<!--<a href="https://wap.jianfeidaren.com/szyd/">-->
+  						<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_sz.jpg" style="display: block;">
+  					
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_sh.jpg" style="display: block;">
+  					<!--<a href="https://wap.jianfeidaren.com/syyd/">-->
+  						<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_sy.jpg" style="display: block;">
+  					
+  					<!--<a href="https://wap.jianfeidaren.com/slyd/">-->
+  						<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_sl.jpg" style="display: block;">
+  					
+  					<!--<a href="https://wap.jianfeidaren.com/hzyd/">-->
+  						<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_hz.jpg" style="display: block;">
+  					
+  					<!--<a href="https://wap.jianfeidaren.com/csyd/">-->
+  						<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_cs.jpg" style="display: block;">
+  					
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_wh.jpg" style="display: block;">
+  					<!--<a href="https://wap.jianfeidaren.com/cdyd/">-->
+  						<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_cd.jpg" style="display: block;">
+  					
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_qd.jpg" style="display: block;">
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_zz.jpg" style="display: block;">
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_6_xa.jpg" style="display: block;">
+  				</div>
+  				<div class="jfxialy_index_2_ln_bottom">
+  					<div class="jfxialy_index_2_ln_bottom_div">
+  						<div class="title">热门营地名额有限</div>
+  					
+  					</div>
+  					<div class="yy"></div>
+  				</div>
+  			</div>
+  			<div class="jfxly_ln_2_7">
+  				<div class="jfxialy_index_2_ln_top">
+  					<div class="tiwen chen">丰富活动告别枯燥</div>
+  					<div class="title">多彩生活  让人恋恋不舍</div>
+  					<div class="yy"></div>
+  				</div>
+  				<div class="jfxly_ln_2_7_1">
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxly_ln_7_1.jpg" style="display: inline;">
+  					
+  				</div>
+  				<!--<div class="jfxialy_index_2_ln_bottom">
+  					<div class="jfxialy_index_2_ln_bottom_div">
+  						<div class="title">犹豫再三不如先咨询了解</div>
+  					
+  					</div>
+  					<div class="yy"><img class="img" src="picture/yyjieshao.jpg"></div>
+  				</div>
+  			</div>
+  			<div class="jfxly_ln_2_10">
+  				<div class="jfxialy_index_2_ln_top">
+  					<div class="tiwen chen">完善售后终身享受</div>
+  					<div class="title">完善制度  消除后顾之忧</div>
+  					<div class="yy"><img class="img" src="picture/yyjieshao.jpg"></div>
+  				</div>
+  				<div class="jfxly_ln_2_10_1">
+  					<div class="jfxly_ln_2_10_1_1">
+  						<img data-original="/templets/default/images_n/jfxly_ln_10_1.png" class="img lazyload" src="picture/jfxly_ln_10_1.png" style="display: inline;">
+  						<div class="title">离营专享<br>减脂方案</div>
+  						<div class="miaoshu">离营定制减脂运动饮食计划，好习惯形影不离</div>
+  						<div class="button_swt" id="jfxly_swt_1"><a href="javascript:void(0)" onclick="openZoosUrl('chatwin','&amp;e=%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425_%25u6DF1%25u5733%25u51CF%25u80A5%25u8FBE%25u4EBA%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425-%25u7533%25u8BF7%25u53C2%25u89C2');return false;" title="客服人员在线,欢迎点击咨询" target="_self">申请参观<span class="span">+</span></a></div>
+  					</div>
+  					<div class="jfxly_ln_2_10_1_1">
+  						<img data-original="/templets/default/images_n/jfxly_ln_10_2.png" class="img lazyload" src="picture/jfxly_ln_10_2.png" style="display: inline;">
+  						<div class="title">持续跟踪<br>监督指导</div>
+  						<div class="miaoshu">教练团队免费监督跟踪；怎么吃怎么练一目了然</div>
+  						<div class="button_swt" id="jfxly_swt_2"><a href="javascript:void(0)" onclick="openZoosUrl('chatwin','&amp;e=%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425_%25u6DF1%25u5733%25u51CF%25u80A5%25u8FBE%25u4EBA%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425-%25u9884%25u7EA6%25u540D%25u989D');return false;" title="客服人员在线,欢迎点击咨询" target="_self">预约名额<span class="span">+</span></a></div>
+  					</div>
+  					<div class="jfxly_ln_2_10_1_1">
+  						<img data-original="/templets/default/images_n/jfxly_ln_10_3.png" class="img lazyload" src="picture/jfxly_ln_10_3.png" style="display: inline;">
+  						<div class="title">终身VIP<br>减肥专享</div>
+  						<div class="miaoshu">终身VIP的减肥服务，减肥达人是你的终身管家</div>
+  						<div class="button_swt" id="jfxly_swt_3"><a href="javascript:void(0)" onclick="openZoosUrl('chatwin','&amp;e=%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425_%25u6DF1%25u5733%25u51CF%25u80A5%25u8FBE%25u4EBA%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425-%25u54A8%25u8BE2%25u4EF7%25u683C');return false;" title="客服人员在线,欢迎点击咨询" target="_self">咨询价格<span class="span">+</span></a></div>
+  					</div>
+  					<div class="jfxly_ln_2_10_1_1">
+  						<img data-original="/templets/default/images_n/jfxly_ln_10_4.png" class="img lazyload" src="picture/jfxly_ln_10_4.png" style="display: inline;">
+  						<div class="title">全天制1V1<br>咨询答疑</div>
+  						<div class="miaoshu">减肥顾问节假日无休制在线答疑解惑，随叫随到</div>
+  						<div class="button_swt" id="jfxly_swt_4"><a href="javascript:void(0)" onclick="openZoosUrl('chatwin','&amp;e=%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425_%25u6DF1%25u5733%25u51CF%25u80A5%25u8FBE%25u4EBA%25u51CF%25u80A5%25u8BAD%25u7EC3%25u8425-%25u7B54%25u7591%25u89E3%25u60D1');return false;" title="客服人员在线,欢迎点击咨询" target="_self">答疑解惑<span class="span">+</span></a></div>
+  					</div>
+  					  
+  				</div>
+  			</div>-->
+			<div class="jfxialy_index_2_ln_4"><!--这里当做7-->
+  				<div class="jfxialy_index_2_ln_top">
+  					<div class="tiwen chen">真诚反馈见证服务</div>
+  					<div class="title">如同家人般的温暖</div>
+  					<div class="yy"></div>
+  				</div>
+  				<div class="jfxialy_index_2_ln_4_1">
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_7_1.jpg" style="display: inline;">
+  				
+  					<img data-original="shenhe3/picture/jfxly_ln_2_3_11.jpg" class="img lazyload" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/jfxialy_index_2_2_ln_7_2.jpg" style="display: inline;">
+  					
+  				</div>
+  			</div>
+  		</div>
+  	</div>
+  	<!--底部开始-->
+	<div class="common_botton">
+	<div class="common_botton_2">
+		
+	</div>
+	<div class="common_botton_1">
+		<div class="common_botton_1_1 bqsy">广州沸腾网络科技有限公司</div>
+		<div class="common_botton_1_1 bah">ICP备案：豫ICP备17029464号-2
+</div>
+	</div>
+	<div class="bottom_nav_box">
+		<div class="bottom_nav_box_div common_ln_640 relative">
+			<div class="bottom_nav_box_ln">
+				
+				<div class="bottom_nav_box_ln_2">
+				<!--电话-->
+				<a href="tel:4008946898" id="jfxly_swt_mfdh">
+				<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/mfdh_bg.png" class="img_bg">
+				<div class="fudong">
+				<span class="span">免费电话</span>
+				<div><img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/mfdh.png" class="img"></div>
+				</div></a>
+				</div>
+				
+			</div>
+			
+		</div>
+	</div>
+	
+
+</div>
+<!--视频-->
+<div class="ship_main">
+	<div class="ship_ceng"></div>
+	<div class="ship_div">
+		<div class="ship_div_1">
+			<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/close_img.png" class="close_img">
+		</div>
+	</div>
+</div>
+
+<script language="javascript" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/lsjs.js.下载"></script><link href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/JS5.css" rel="stylesheet" type="text/css"><div id="LRdiv0" style="display:none;"></div><div id="LRdiv1" style="display:none;"></div><div id="LRdiv2" style="display:none;"></div><div id="LRdiv3" style="display:none;"></div><link href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/JS5(1).css" rel="stylesheet" type="text/css"><div id="LRdiv0" style="display:none;"></div><div id="LRdiv1" style="display:none;"></div><div id="LRdiv2" style="display:none;"></div><div id="LRdiv3" style="display:none;"></div><link href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/JS5(2).css" rel="stylesheet" type="text/css"><div id="LRdiv1" style="display:none;"></div>
+
+ 
+
+
+
+</body></html>
