57b6f1b26a3facdcf94008421e0f682cb4a647df
何文通 <396595399@qq.com>
2020-03-27 17:19:48 +0800

    ts

---
+++ xw/shenhe4.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!-- saved from url=(0064)https://ad.nearme.com.cn/lp/200019754/html/landpage.htm?im=$im1$ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!--<base href="/">--><base href=".">
+    
+    <meta content="width=device-width,initial-scale=1.0,maximum-scale=1,minimum-scale=1" name="viewport">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <meta content="address=no" name="format-detection">
+
+    <meta name="full-screen" content="yes">
+    <meta name="x5-orientation" content="portrait">
+
+    <title></title>
+
+    <link rel="stylesheet" href="./shenhe4/global.css" type="text/css">
+    <script src="./shenhe4/jquery.min.js"></script>
+    <script src="./shenhe4/swiper.min.js"></script>
+    <script src="./shenhe4/popwindow.js"></script>
+    <script src="./shenhe4/loading.js"></script>
+    <script src="./shenhe4/toast.js"></script>
+    <script src="./shenhe4/utils.js"></script>
+    <script src="./shenhe4/formal.js"></script>
+</head>
+
+<body>
+
+
+
+<div class="main" style="background-color:#FFF">
+
+    <div style="padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px" class="Image-container common-container">
+<div id="CbImage_15845347810006febb9c1-1a4a-4b9c-dc19-80407ae0029e">
+    <img src="./shenhe4/6klqqkeo0ejiao313r9c52k09p.jpg">
+
+</div>
+
+<script>
+    $("#CbImage_15845347810006febb9c1-1a4a-4b9c-dc19-80407ae0029e img").on("click", function (e) {
+        var eventData = {
+            linkType: 'none',
+            linkUrl: '',
+            phone_number: '',
+            sms_number: ''
+        };
+        componentItemClick(0, 'Image', eventData);
+    });
+</script>
+
+    </div>
+</div>
+
+
+
+<script src="./shenhe4/statistics-v2.js"></script><iframe src="./shenhe4/adv.html" name="pipe" style="display: none;"></iframe>
+
+</body></html>
