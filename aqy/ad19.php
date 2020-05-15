005198d899cfe6f32c27c6a309913a1837f96d41
何文通 <396595399@qq.com>
2020-05-15 19:50:56 +0800

    aqy => cza2

---
+++ aqy/ad19.php
@@ -0,0 +1,295 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, minimal-ui">
+		<title>我没节食，没运动,不再痛苦与折磨！ </title>
+		<link rel="stylesheet" type="text/css" href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/css/index.css">
+  <script>
+ var stxlwx = "<?php echo $stxlwx?>"
+</script>
+	<body>
+		<div class="g">
+			<div class="Header">
+				<div class="contnet">
+					<div class="Boundary" deep="3">
+						<h3>我没节食，没运动,不再痛苦与折磨！ <span class="red">暴瘦57.2斤</span>，亲眼看着黄色油脂排出拯救了自己的人生！快看吧，以免删除"</h3>
+						<p style="text-indent:0;font-size:14px;color:#666;font-weight:bold"><?php echo date("Y-m-d 13:20:47",strtotime("-7 days"))?></p>
+						<h4 class="bt red">"下面是我的客户反馈——谢谢各位客户给我做的广告方案"</h4>
+						<p>自从在胖友圈发了自己减肥成功的照片，我微信的震动就没停过，评论被挤爆，夸我<span class="red">美了瘦了</span>，更多的是不可思议，追问我<span class="red">减肥的秘诀</span>，求助减肥方法，其实人都有私心，这么好的事真不想说，我能走到今天这步，没啥可说的，感谢我的专属导师救了我，把我在痛苦的边缘拉回来，让我不但瘦的如此健康，还改变了自己的一生！</p>
+						<p>跟对人找对方法真的瘦让平板支撑，节食什么的都去死吧，根本瘦不下来感谢我的导师帮我，果真，减肥是一切美好的开始</p>
+						<dd class="bt1">本人长得不好看 不过瘦了还挺好看</dd>
+						<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/01.jpg" alt="">
+						<dd class="bt1">让效果说话&nbsp; 向脂肪宣战</dd>
+
+						<p>我叫赵茹，由于久坐办公室，29岁之后，体重开始直线上升，当时<span class="red">体重刚到121.4斤，过了34岁之后，我的体重从120斤直接涨到136斤，最可怕的是生了一胎，紧接着跟老公要了二胎，过了哺乳期后，那时候我的体重已经达到了175.6斤</span>，看着镜子里的自己我开始害怕，都说过了三十岁之后的女人身体会开始诱发各种疾病，像三高那些，尤其是我这样的肥胖人群。</p>
+						<p>慢慢的我之前的衣服开始一件都穿不上，脖子越来越短，开始穿宽大的衣服，想系鞋带弯不下腰，大肚腩大粗腿让我恶心，老公喊我“大妈”那一刻起，我受到了伤害，我决心开始减肥，为了瘦，跑步节食只喝水，吃各种产品，用过的产品不下15种，能试的我都试了，很多无良商家为了利益不择手段不顾及质量，害得我不是拉肚子就是月经不调时常吃了呕吐恶心，有次差点进了医院，减肥这条路我知道有多难走，我也知道胖能让一个人多自卑，我走过的弯路你们就不要走了。这次偶然机会让我意外惊喜，也没有像以前一样，吃了以后出现什么不良反应，相反的，感觉整个人的身体都得到了调理，所以<span
+							 class="red">没有用我绝对不会推荐给你们的！</span></p>
+						<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/02.jpg" alt="">
+						<dd class="bt1">感谢导师的出现，拯救了我的人生</dd>
+						<p>"其实听了我说的这些，你们也别嘲笑我,我真的曾经一
+							度想过要放弃，我觉得我已经坚持不下去了，再这样下去我会死的，那时候已经到了走路就喘，下不了楼出不了门，心脏骤停，弯不下腰，只能每天躺在床上等死的地步，看着曾经要好的闺蜜朋友一个个嘲笑我，议论我的嘴脸，看着老公跟我一遍遍提出离婚未果，最后直接带女人回了家里，父母看着我不停的叹气，满头白发，看着出生不久的宝宝整天哇哇哭，这一切我都看在眼里，终于我觉得我不能这样等死了我要开始逆袭，我还有爱我的家人，只要我还有一口气我就一定要减肥成功，我让家人拜托亲戚朋友寻找减肥方法，给塞了钱，送了礼，大概持续了一个月左右，妈妈带来的消息让我加这个导师的微信号试一试，病急乱投医，我当时真的
+							是没有办法了，反正都要死，那就试试，为了瘦，我赌一把（我把老师的微信号分享给大家，大家可以咨询一下）"</p>
+						<p style="text-indent:0;text-align:center;font-size:18px;"><span style="background:yellow;color:red;font-weight:bold;padding:5px;border-radius:10px;font-size:20px"
+							 oncopy="copywx()" id="wxchid_wzm"><span class="zaax-wxh wx1" id="goto_wxchto" onClick="copywx()"><?php echo $stxlwx?></span></span>（非微信公众号）</p>
+						<dd class="bt1" style="font-size: 18px;">跟着老师两个月，<span class="red">我成功逆袭了</span>，我做到了</dd>
+						<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/03.jpg" alt="">
+						<p>"<span class="red">我把我瘦下来的样子发到了胖友圈</span>，之前也说到了，人都有私心，真的瘦了不想分享给大家，生怕大家瘦的比我多、比我美。<span class="red">其实不是想炫耀什么，就想证明我自己，让全世界觉得我可以，我也配拥有童话般的爱情和令人羡慕的友情</span>。别看我这三十多岁了，但我真的觉得，女生想让自己变
+							漂亮变瘦是很正常的事情，你瘦了之后全世界都会对你好，我就是这样，所以我理解我必须改变自己，遇到老师遇到它，我可以说我非常幸运了，它改变了我即将垂死的一生，让我老公朋友，家人回到我的身边，"</p>
+						<p class="red">"就是这么个东西，给大家看看，我历时两个月瘦了57.2斤，从 此改变了我赵茹的一生，你瘦下来的样子</p>
+
+						<p class="red">" 相当于喝100杯酸奶，吃20斤黄瓜做12次有氧运动，相当于做15次经络排毒，8次精油按摩。刮油、除油效果堪比抽脂机，故在饮用量上一定要注意，一旦超标很容易瘦成皮包骨！</p>
+						<p><span class="red">排出黄色油脂</span>，每天都能看得见，是有点恶心，但这就是真真实实附在我们体内，十二指肠上的油脂，我们自己是无法将体内垃圾排出的，只能靠这个秘方，<span class="red">能做到无副作用，健康排出体内垃圾</span></p>
+						<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/05.jpg" alt="">
+						<p>遇到导师，遇到它我可以说是非常幸运了，好东西试了才知道，你不试磨磨唧唧那就不用减肥了，发胖是对自己的不负责任，连自己体重都控制不了的人还能做什么，走出第一步才有第二步，相信自己，不懂的或者有减肥方面的困惑的朋友，可以咨询导师，导师会根据每人的情况定制不同的减肥方案。</p>
+						<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/06.jpg" alt="">
+						<p>家住汕头的美娟，体重最重时162斤，用了神奇秘方，瘦掉58斤，大肚腩瞬间瘪了下去，腰形出来了，形象指数窜上一大截！消灭大肚子，“秘方君”绝对专业哟！</p>
+						<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/07.jpg" alt="">
+						<p>"
+							天津的小宁说，她用了神奇秘方当天，竟排出至少2斤“巨便”，还不腹泻，排完小肚子塌一半，又软又舒服。上秤一量，体重当时掉了二斤半。她自己又惊又怕，不明白肠道里怎么会有这么多脏东西？赶紧通过微信咨询导师。导师告诉她，她排出的都是身体积存的宿便、毒素油脂垃圾。继续用，保证她变瘦变漂亮。那以后，小宁几乎一天瘦一圈，一
+							斤，一斤半，2斤，体重天天都在掉，原本像山一样高耸的大肚腩平了，腿细了，腰形出来了……用了短短<span class="red">30天</span>，整整<span class="red">瘦掉26斤</span>，原本干燥的皮肤也变得水润"</p>
+						<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/08.jpg" alt="">
+						<ul>
+							<li>郭云、白领</li>
+							<li>减肥时长：53天</li>
+						</ul>
+						<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/09.jpg" alt="">
+						<p>我儿子是青春期长胖的，长胖之后，他变得非常自卑，不喜欢交朋友，整天宅在家里。做母亲的我，看在眼里，非常难受。有一天弟妹找我玩，没想到她减肥了，天啦!不可思议，瘦的太快了。她之前比我儿子还要胖，听她一说，我赶紧让她帮我联系瘦身顾问，因为儿子马上要相亲了，可总感觉他郁郁寡欢的，我明白，肥胖对他的心理压力很大，后来在瘦身顾问的指导下，用了50多天，令我和儿子感到惊喜的是，不但变瘦了。也变帅气了，而且儿子现在整个人也更加自信了。做为母亲的我，心里那个高兴啊，感谢瘦身顾问让我儿子有这么大的变化。很多人使用减肥方法反馈，不仅瘦了，<span
+							 class="red">还不用节食，不用运动，轻轻松松地就能减肥。不用再饿肚子和大量运动，减肥也不怕反弹了</span>。</p>
+						<dd class="bt2">适合人群</dd>
+						<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/010.jpg" alt="">
+						<p class="red">"
+							目前全国众多肥胖人士，热衷于这个减肥方法，平均2天开始减重，40天平均减重45.8斤，腰围平均缩减5-12公分，且无一例不良反应者。使用该方法最多的是生完小孩刚断奶的女性，用了秘方，瘦的特别快，有的人不到一个月就把怀孕长的肉都减掉了。还有些嘴馋不爱运动的馋人、懒人，一个月也可以减掉20斤左右。有些爱美的小姑娘，本身不太胖，也想瘦个五斤八斤的，更是轻轻松松就瘦下来了。</p>
+						<p style="text-indent:0;text-align:center;font-size:18px;">想瘦请点击复制加个人微信号</p>
+						<p style="text-indent:0;text-align:center;font-size:18px;"><span style="background:yellow;color:red;font-weight:bold;padding:5px;border-radius:10px;font-size:20px"
+							 oncopy="copywx()" id="wxchid_wzm"><span class="zaax-wxh wx1" id="goto_wxchto" onClick="copywx()"><?php echo $stxlwx?></span></span>（非微信公众号）</p>
+						<!-- <h3 style="color:red;font-weight:bold;font-size:18px;text-align:center">请勿添加任何相关公众号！谨防诈骗！</h3>
+ <p style="text-indent:0;text-align:center;font-size:16px;background:red;border-radius: 10px;color:#fff;font-weight:bold">想瘦请长按复制个人微信号:<span class="zaax-wxh"></span>（非微信公众号）</p>
+        <dd class="bt1">请勿添加任何相关公众号！</dd> -->
+						<p class="red">郑重提醒：比肥胖本身更可怕的是有害身体的减肥方法。 减肥，品牌决定品质，品质决定效果，减肥认准大品牌，全国百强健康集团祝您早日瘦身成功。"</p>
+					</div>
+
+				</div>
+				<div class="bian">
+					<p class="pl">热门评论</p>
+					<div class="pltxt">
+						<div class="fl fltu">
+							<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/p1.jpg" class="pltu">
+						</div>
+						<div class="fl pig">
+							<div class="dog">
+								<p class="fl ys">冷杉与空&nbsp;&nbsp;甘肃兰州</p>
+							</div>
+							<p class="txt0">第一次用的时候还很怀疑，但是第二天就瘦了1斤，吓了一跳。一个月下来瘦了21斤！很开心！！！</p>
+							<div class="rig zan1">
+								<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/zan.jpg" class="zan">
+								<p class="zanx"> 6804 </p>
+							</div>
+						</div>
+					</div>
+					<div class="pltxt">
+						<div class="fl fltu">
+							<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/p2.jpg" class="pltu">
+						</div>
+						<div class="fl pig">
+							<div class="dog">
+								<p class="fl ys">哈利路亚&nbsp;&nbsp;广西桂林</p>
+							</div>
+							<p class="txt0">买的时候想着反正也不贵，就抱着试试看的态度，不到两千块钱的强效套餐，一个月瘦了27斤，打算再吃一个月，体重保持在两位数就好了。</p>
+							<div class="rig zan1">
+								<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/zan.jpg" class="zan">
+								<p class="zanx"> 5274 </p>
+							</div>
+						</div>
+					</div>
+					<div class="pltxt">
+						<div class="fl fltu">
+							<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/p3.jpg" class="pltu">
+						</div>
+						<div class="fl pig">
+							<div class="dog">
+								<p class="fl ys">凉凉&nbsp;&nbsp;北京东城</p>
+							</div>
+							<p class="txt0">生完宝宝后，一直瘦不下来，肚子几圈肥肉，真是烦死人了。后来在宝妈群上看到这
+								个文章，大家都说不错，抱着怀疑的态度尝试了一段时间，没想到真的瘦了。才花一千多块钱，减了24斤。太惊喜了。</p>
+							<div class="rig zan1">
+								<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/zan.jpg" class="zan">
+								<p class="zanx">346 </p>
+							</div>
+						</div>
+					</div>
+					<div class="pltxt">
+						<div class="fl fltu">
+							<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/p4.jpg" class="pltu">
+						</div>
+						<div class="fl pig">
+							<div class="dog">
+								<p class="fl ys">桃花源&nbsp;&nbsp;江西南昌</p>
+							</div>
+							<p class="txt0">神奇的秘方真的挺厉害，我自小就比较胖，小时候胖点可爱，长大后才发现可怕，影响到我生活上的一切！神奇秘方，竟然真帮我瘦下来了。</p>
+							<div class="rig zan1">
+								<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/zan.jpg" class="zan">
+								<p class="zanx"> 964 </p>
+							</div>
+						</div>
+					</div>
+					<div class="pltxt">
+						<div class="fl fltu">
+							<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/p5.jpg" class="pltu">
+						</div>
+						<div class="fl pig">
+							<div class="dog">
+								<p class="fl ys">幺儿宝贝&nbsp;&nbsp;山东济南</p>
+							</div>
+							<p class="txt0">我从小就是吃货，主要是局部性肥胖，就大腿和手臂粗，竟然也可以通过改变体质瘦下来，太厉害了！</p>
+							<div class="rig zan1">
+								<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/zan.jpg" class="zan">
+								<p class="zanx"> 2556 </p>
+							</div>
+						</div>
+					</div>
+					<div class="pltxt">
+						<div class="fl fltu">
+							<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/q1.jpg" class="pltu">
+						</div>
+						<div class="fl pig">
+							<div class="dog">
+								<p class="fl ys">MIN海棠朵朵&nbsp;&nbsp;甘肃兰州</p>
+							</div>
+							<p class="txt0">全靠老师的帮忙！我终于减到下来了，咨询还是免费的，太给力了。</p>
+							<div class="rig zan1">
+								<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/zan.jpg" class="zan">
+								<p class="zanx"> 3528 </p>
+							</div>
+						</div>
+					</div>
+					<div class="pltxt">
+						<div class="fl fltu">
+							<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/q2.jpg" class="pltu">
+						</div>
+						<div class="fl pig">
+							<div class="dog">
+								<p class="fl ys">燕清1020&nbsp;&nbsp;山东烟台</p>
+							</div>
+							<p class="txt0">自从上大学后，体重一直往上飙升，就没下过100斤。女生都是爱美的，3位数的体重真的令我很在意。在老师的帮助下，坚持用神奇秘方，一个月的时间体重降到2位数，真的太有效了！</p>
+							<div class="rig zan1">
+								<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/zan.jpg" class="zan">
+								<p class="zanx"> 2763 </p>
+							</div>
+						</div>
+					</div>
+					<div class="pltxt">
+						<div class="fl fltu">
+							<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/q3.jpg" class="pltu">
+						</div>
+						<div class="fl pig">
+							<div class="dog">
+								<p class="fl ys">你笑起来很美&nbsp;&nbsp;江苏苏州</p>
+							</div>
+							<p class="txt0">我生完宝宝以后胖了很多，而且一直减不下来，就是用神奇秘方瘦下来的</p>
+							<div class="rig zan1">
+								<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/zan.jpg" class="zan">
+								<p class="zanx"> 2221 </p>
+							</div>
+						</div>
+					</div>
+					<div class="pltxt">
+						<div class="fl fltu">
+							<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/q4.jpg" class="pltu">
+						</div>
+						<div class="fl pig">
+							<div class="dog">
+								<p class="fl ys">似水风情&nbsp;&nbsp;浙江杭州</p>
+							</div>
+							<p class="txt0">从小就是胖妞，读小学时经常被人欺负，真的很难过，性格也变得自卑。认识老师后才下定决心减肥。真的很感谢老师，减肥不仅是变美那么简单，是整个人生的蜕变。</p>
+							<div class="rig zan1">
+								<img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/zan.jpg" class="zan">
+								<p class="zanx">4201 </p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<p style="text-align:center;font-size:12px;color:#666">
+					<!-- 版权所有：河南钰丝雅生物科技有限公司 -->
+				</p>
+				<div style="height:50px"></div>
+
+
+				<div class="fuchuang">
+					<!-- <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/222.jpg" style="position: absolute;top:-12px;left:3px;display:block;width:58px;border-radius:50%"> -->
+
+					<a href="weixin://" onclick='_czc.push(["_trackEvent", "微信号复制", "复制成功", "微信号"])' class="wxdk">
+						<div class="tu3" id="goto_wxchto" onClick="gotowx()" style="float:right;margin-right:5px;padding:6px; background-color: red;
+        color: #fff;border-radius: 5px;text-align:center;margin-top:11px;">微信咨询</div>
+					</a>
+					<div style="float:left;color:#fff;margin-left:5px; padding-top:5px;color:#000;" class="nnn">
+						<p style="margin:0;padding-top:3px">复制加微信好友<span class="wxh" id="wxchid_cop" oncopy="copywx()" style="background-color:red;padding:3px 6px;color:#fff;font-weight:bold;border-radius:2px;"><?php echo $stxlwx?><span
+								 class="zaax-wxh wx1" id="goto_wxchto" onClick="copywx()">
+								</span></span></p>
+						<p style="font-size:0;"><em id="click_copy_wxchid"><span class="zaax-wxh wx1" id="goto_wxchto" onClick="copywx()">
+								</span></em></p>
+						<p style="font-weight:bold;margin-top:0px;">减肥这是最后一次（点击微信号复制）</p>
+					</div>
+
+				</div>
+
+			</div>
+			<script src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/js/jquery.min.js"></script>
+			<script>
+			$.ajax({
+						url:'http://heven.top:8000/app/log',
+						method:'post',
+						data:{'type':'load','local_url':window.location.href,'weixin':"<?php echo $stxlwx?>"},
+					})
+				/*点击复制微信号*/
+				function copywx() {
+					let oInput = document.createElement("input");
+					oInput.value = "<?php echo $stxlwx?>";
+					oInput.readOnly="readOnly";
+					document.body.appendChild(oInput);
+					oInput.select(); // 选择对象
+					oInput.setSelectionRange(0, 20); //兼容ios-safari核心代码
+					document.execCommand("Copy"); // 执行浏览器复制命令
+					$.ajax({
+						url:'http://heven.top:8000/app/log',
+						method:'post',
+						data:{'type':'copy','local_url':window.location.href,'weixin':"<?php echo $stxlwx?>"},
+					})
+					alert("微信号复制成功，打开微信添加！");
+					window.location.href = 'weixin://';
+				
+				
+				}
+			
+				function gotowx() {
+					let oInput = document.createElement("input");
+					oInput.value = "<?php echo $stxlwx?>";
+					oInput.readOnly="readOnly";
+					document.body.appendChild(oInput);
+					oInput.select(); // 选择对象
+					oInput.setSelectionRange(0, 20); //兼容ios-safari核心代码
+					document.execCommand("Copy"); // 执行浏览器复制命令
+					$.ajax({
+						url:'http://heven.top:8000/app/log',
+						method:'post',
+						data:{'type':'copy','local_url':window.location.href,'weixin':"<?php echo $stxlwx?>"},
+					})
+					alert("微信号复制成功，打开微信添加！");
+					window.location.href = 'weixin://';
+					
+					$.ajax({
+						url:'http://heven.top:8000/app/log',
+						method:'post',
+						data:{'type':'gowechat','local_url':window.location.href,'weixin':"<?php echo $stxlwx?>"},
+					})
+			
+				
+				}
+			</script>
+
+	
+		</div>
+	</body>
+<script type="text/javascript" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/js/z_stat.js"></script>
+</html>
