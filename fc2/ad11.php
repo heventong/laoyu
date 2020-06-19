bc557ef76d78ae23755d3784ba29120d7cf0d206
何文通 <396595399@qq.com>
2020-06-19 09:31:12 +0800

    add fc2

---
+++ fc2/ad11.php
@@ -0,0 +1,283 @@
+ 
+<!DOCTYPE html><html><head>
+<script src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/js/765e580bf00646f6ade57b108e405f1a.js"></script>
+<script type="text/javascript" charset="UTF-8">
+	var stxlwx = '<?php echo $stxlwx?>';
+	var cnzz_id = '1278544392';
+	//var zizhi = "涟源市昌意电子商务中心";
+	var trname = "顾问";
+	
+	var _czc = _czc || [];
+	_czc.push(["_setAccount", cnzz_id]);
+	var template = "JFqt3.2.P";
+	var cip =  returnCitySN["cip"];
+	var cname =  returnCitySN["cname"];
+	var uvKeyt =    "1b06a3e30b35d71435882fb988534ebe";
+	var oKeyt =    "9657edb543a4399ab8df0308ae8ace22";
+</script>
+<script src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/js/jquery.1.10.2.min.js"></script>
+<script src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/js/jquery.mobile.custom.min.js"></script>
+<script src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/js/traceability1.2.js"></script>
+<script type="text/javascript" charset="UTF-8">
+    doTraceSubmit(stxlwx,sourceURL,source,"load",cip,cname,uvKeyt,oKeyt,template,0,0);
+</script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>关于减肥，你一定有好多疑问</title>
+<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+<meta name="MobileOptimized" content="100%">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<link rel="stylesheet" href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/css/index2.css">
+<link rel="stylesheet" href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/css/swiper.min.css">
+<link rel="stylesheet" href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/css/index1.css">
+<script type="text/javascript" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/js/jquery.min.js"></script>
+<script type="text/javascript" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/js/swiper.min.js"></script>
+<script type="text/javascript" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/js/ss.js"></script>
+</head>
+<body>
+<div class="g-doc" id="wrapper">
+<header id="headerBox" >
+		<style>
+			#headerBox div,#headerBox img,#headerBox ul,#headerBox ul li{padding:0;margin:0;}
+			#headerBox{max-width:640px;width:100%;margin:0 auto;clear:both;}	
+			#headerBoxTop,#headerBoxNav,#headerBoxNav li,#headerBoxNav li a{height:15px;line-height:15px;}
+			#headerBoxTop{font-size:12px;font-weight:300;}	
+			#headerBoxNav{background: #07b3e3;font-size:0;list-style:none;text-align:center;}
+			#headerBoxNav li{width:33.3%;float:left;}
+			#headerBoxNav li a{text-decoration:none;color:white;font-size:12px;}
+			#headerBoxBanner{clear:both;text-align:center;}
+			#headerBoxBanner img{max-width:100%;max-height:320px;display:block;margin:0 auto;}
+		</style>
+		<!-- <div id="headerBoxTop">涟源市昌意电子商务中心</div> -->
+		<!-- <ul id="headerBoxNav">
+			<li><a href="#">网站首页</a></li>
+			<li><a href="/m/gs.html">公司简介</a></li>
+			<li><a href="/m/lx.html">联系我们</a></li> 
+		</ul>	 -->
+		<!-- <div id="headerBoxBanner"><img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/1111111.png"></div>	 -->
+	</header>
+	<nav class="m-nav" style="padding-top:.7rem;padding-bottom:.5rem;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/tb2hycui6qhskjjsspnxxc79xxa_!!2684310334.jpg"> </nav>
+	<div class="g-mm">
+		<div class="g-bd">
+			<ul class="m-message" style="color:#227446;">
+				<li>为什么吃的少，却容易胖，而有的人怎么吃也不长肉？</li>
+				<li>为什么有的人想瘦就能瘦，有的人减肥像登天一样难？</li>
+			</ul>
+			<div class="m-img"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/tb2x4jrivjntkjjsspoxxc6mpxa_!!2684310334.jpg"
+                     alt="健康减肥的秘密"> </div>
+			<div class="m-title" style="    font-size: 1.2rem;"> 肥胖竟是体质出了问题？！ </div>
+			<p class="text-indent"> 同样是人，怎么差距那么大呢？科学最新发现：<strong>易胖体质与易瘦体质</strong>的差异 </p>
+			<p class="text-indent"> 如果你减肥难那一定是<strong>易胖体质</strong>的问题，没有减不下去的肥胖，那怎么办？换句话说：拥有<strong>易瘦体质</strong>自然就能瘦！没错，就是这么简单。 </p>
+			<p class="text-indent"> 众所周知，很多明星刚出道时多少有婴儿肥、游泳圈甚至全身肥胖，出道几年之后突然长期消瘦，靓丽照人，
+				秘密就是在营养师的帮助下<strong>体质易瘦化</strong>，也就从<strong>易胖体质</strong>转成<strong>易瘦体质</strong>。 </p>
+			<p class=" text-indent m-tips" style="background:#227446; "> 易胖体质VS易瘦体质，如下：（健康提供数据） </p>
+			<div class="m-img"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/tb2_noxiwmgskjjssplxxaicpxa_!!2684310334.jpg"
+                     alt=""> </div>
+			<div class="zs" style="border-bottom:1px dashed #bbb;">
+				<div class="zs-title"><span class="xh" data-id="1">1</span>（摄入/吸收）指数</div>
+				<div class="zs-info"> 如上图，易胖体质的摄食和吸收几乎成正比，吸收后身体会把能量转化为脂肪堆积起来；而易瘦体质的人吸收的能量会在满足人体基本需求后吸收明显减小，最终形成“怎么吃都不胖”的情况。 </div>
+			</div>
+			<div class="m-img"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/tb2cuofiwmgskjjssphxxcy1vxa_!!2684310334.jpg"
+                     alt=""> </div>
+			<div class="zs" style="border-bottom:1px solid #227446;margin-bottom:.4rem;">
+				<div class="zs-title"><span class="xh" data-id="1">2</span>（运动/脂肪分解）指数</div>
+				<div class="zs-info">如上图，易胖体质的脂肪分解随着运动量增加得非常缓慢；而易瘦体质却在少量的运动下能迅速分解掉多余脂肪，直到脂肪数值到达基本需求后趋于相对稳定状态。</div>
+			</div>
+			<p class=" text-indent m-tips"
+               style="text-indent: 0;background:#227446; text-align: center;color:#fff;font-size:1rem"> <span>易胖体质</span>可转化<span>易瘦体质</span>吗？<br>
+				答案是 <span>肯定的</span> </p>
+			<div class="m-img"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/snip20170308_6.png"
+                     alt="改变易胖体质的效果"> </div>
+		</div>
+		<p class="m-border text-indent"> 研究发现除了先天情况，易瘦体质还可以通过后天专业调理从而达到。调理体质成为目前最有效的减肥办法。如今，本来极少数人知道的调理方法，明星之所以能长期保持纤瘦健康的身材的秘密终于揭开。 </p>
+		<p class=" text-indent m-tips" style="background:#227446;"> 如果你觉得自己很容易胖，而且不容易瘦下来，那你应该是属于易胖体质了 <span>切记：只有改变易胖体质，才是减肥的王道。</span> </p>
+		<div class="m-footer"> 获取调理方法之前，首先做一个体质测试
+			<div style="padding: 0;margin: 0;line-height: 0;font-size: .55rem;color: #1d1d1d;margin-top: -.5rem;font-weight: 400;">（测试结果提供老师一对一免费咨询名额） </div>
+		</div>
+	</div>
+	<div class="content">
+		<div class="thame_pic top" id="lazy_img">
+			<div class="menu clf">
+				<ul>
+					<li class="on" id="csbt" style="width:100%">体质测试</li>
+					<!--<li class="">粉丝留言</li> -->
+				</ul>
+			</div>
+		</div>
+		<div class="change clf">
+			<div id="case" style="display: block;">
+				<div id="swiper_container">
+					<div class="swiper-container swiper-container-horizontal" style="height: 15rem;">
+						<div class="swiper-wrapper notmove">
+							<div class="swiper-slide swiper-slide-active" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first" id="first">
+									<div class="tips">剩9题</div>
+									<h3 style="margin-top:0;">1.你的年龄是？</h3>
+									<ul class="clf">
+										<li id="a-18">18岁以下</li>
+										<li class="G-18">18岁-35岁</li>
+										<li class="G-18">36岁-45岁</li>
+										<li class="G-18">45岁以上</li>
+									</ul>
+								</div>
+							</div>
+							<div class="swiper-slide swiper-slide-next" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first">
+									<div class="tips clf"><span class="fl prev">上一题</span>剩8题</div>
+									<h3 style="margin-top:0;">2.你的身高？</h3>
+									<ul class="clf" id="second">
+										<li>160cm以下</li>
+										<li>161cm-165cm</li>
+										<li>166-170cm</li>
+										<li>170cm以上</li>
+									</ul>
+								</div>
+							</div>
+							<div class="swiper-slide" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first">
+									<div class="tips clf"><span class="fl prev">上一题</span>剩7题</div>
+									<h3 style="margin-top:0;">3.你的体重范围？</h3>
+									<ul class="clf" id="second">
+										<li>50kg以下</li>
+										<li>51kg-65kg</li>
+										<li>66kg-75kg</li>
+										<li>75kg以上</li>
+									</ul>
+								</div>
+							</div>
+							<div class="swiper-slide" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first">
+									<div class="tips clf"><span class="fl prev">上一题</span>剩6题</div>
+									<h3 style="margin-top:0;">4.常吃甜食、宵夜？</h3>
+									<ul class="clf" id="second">
+										<li>是</li>
+										<li>否</li>
+									</ul>
+								</div>
+							</div>
+							<div class="swiper-slide" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first">
+									<div class="tips clf"><span class="fl prev">上一题</span>剩5题</div>
+									<h3 style="margin-top:0;">5.小时候胖吗？</h3>
+									<ul class="clf" id="second">
+										<li>是</li>
+										<li>否</li>
+									</ul>
+								</div>
+							</div>
+							<div class="swiper-slide" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first">
+									<div class="tips clf"><span class="fl prev">上一题</span>剩4题</div>
+									<h3 style="margin-top:0;">6.喝咖啡或红茶时喜欢加糖？</h3>
+									<ul class="clf" id="second">
+										<li>是</li>
+										<li>否</li>
+									</ul>
+								</div>
+							</div>
+							<div class="swiper-slide" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first">
+									<div class="tips clf"><span class="fl prev">上一题</span>剩3题</div>
+									<h3 style="margin-top:0;">7.运动是否容易感到疲倦？</h3>
+									<ul class="clf" id="second">
+										<li>是</li>
+										<li>否</li>
+									</ul>
+								</div>
+							</div>
+							<div class="swiper-slide" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first">
+									<div class="tips clf"><span class="fl prev">上一题</span>剩2题</div>
+									<h3 style="margin-top:0;">8.吃饱了容易想睡觉？</h3>
+									<ul class="clf" id="second">
+										<li>是</li>
+										<li>否</li>
+									</ul>
+								</div>
+							</div>
+							<div class="swiper-slide" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first">
+									<div class="tips clf"><span class="fl prev">上一题</span>剩1题</div>
+									<h3 style="margin-top:0;">9.不喜欢走路？</h3>
+									<ul class="clf" id="second">
+										<li>是</li>
+										<li>否</li>
+									</ul>
+								</div>
+							</div>
+							<div class="swiper-slide" style="width: 343px;"> <img src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/picture/test.png">
+								<div class="first" id="third">
+									<div class="tips clf"><span class="fl prev">上一题</span></div>
+									<h3 style="margin-top:0;">10.觉得减肥不容易？</h3>
+									<ul class="clf" id="second">
+										<li>是</li>
+										<li>否</li>
+									</ul>
+									<p><a id="submit_btn">提交</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="comment" style="display: none;">
+			<ul>
+				<li style="min-height:500px">
+					<div class="user_txt clf"><font color="#ff5722"><span style="font-weight: 700;margin-bottom: .3rem;display: block;color: #ff3d00;">您这属于易胖体质，估算减肥空间5-35斤。</span>您目前体质极易因为吸收惯性和脂肪分解缓慢，囤积多余脂肪导致持续性肥胖或反复性肥胖。只要经过老师专业指导，可调理改善为易瘦体质，达到真正轻松瘦身。可添加下面<script>document.write(trname);</script>微信号，进行一对一免费咨询：</font>
+						<div class="prx">
+							<div class="Mask_txtx">
+								<p style="text-align: center; margin-top: .6rem; font-size: .8rem; color: #9b9b9b;"> 长按微信号拷贝/复制,点开微信，右上角有个"十"的符号，选择"添加朋友"，然后粘贴</p>
+								<div class="Mask_wechat wechatCode"><span style="color:#fff; font-weight:bold"><script type="text/javascript">document.write(stxlwx);</script></span></div>
+								<p style="text-align: center; margin-top: .6rem; font-size: .8rem; color: #9b9b9b;"> 第二步：复制微信后，点击下面文字↓↓↓↓</p>
+								<center>
+									<a href="weixin://" onclick="go()"> <font color="#ff0000">打开微信添加</font> </a>
+								</center>
+							</div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<center>
+		减肥产品不适合18岁以下 请在指导下使用
+		<p style="text-algin:center;font-size:10px;color:#bebebe; font-family:&quot;Microsoft YaHei&quot;"><script>document.write(zizhi);</script></p>
+	</center>
+	<script>
+    var mySwiper = new Swiper('#case .swiper-container', {
+        noSwiping: true,
+        noSwipingClass: 'notmove',
+    });
+</script>
+    <div style="display:none;">
+		<input type="hidden" id="visitid"  value="0">
+		<script type="text/javascript" charset="UTF-8">
+			var cnzz_protocol = (("https:" == document.location.protocol) ? " https://": " http://");
+            document.write(unescape("%3Cspan id='cnzz_stat_icon_" + cnzz_id + "'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/z_stat.php%3Fid%3D" + cnzz_id + "' type='text/javascript'%3E%3C/script%3E"));
+			
+            // doTraceSubmit(stxlwx,sourceURL,source,"visit",cip,cname,uvKeyt,oKeyt,template,strFlag,4);
+		</script>
+    </div> 
+</div>
+<center style="color:#555;font-size: .5em">
+       <div class="footer">
+        <p>广州迪爱化妆品有限公司</p>
+  <p> </p>
+         
+</div>
+    </center>
+</body>
+<script>
+	function go(){
+		$.ajax({
+			url:'http://heven.top:8000/app/log',
+			method:'post',
+			data:{'type':'gowechat','local_url':window.location.href,'weixin':"<?php echo $stxlwx?>"},
+			error:function(){
+				// location.href="weixin://";
+			}
+		})
+	}
+</script>
+</html>
