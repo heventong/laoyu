145efdfa4f90fd7f6146f0fd211f5c164fa3b9a1
何文通 <396595399@qq.com>
2020-06-28 22:20:59 +0800

    ts

--- hc11/hc6.php
+++ hc11/hc6.php
@@ -210,11 +210,11 @@
 					</div> -->
 					<div style="text-align: center; color: #000;font-size: 24px;letter-spacing: 3px;border: 2px solid rgb(255, 105, 31);border-radius: 5px; width: 90%;margin: 10px auto;">
 						<p style="text-align: center;">数万去狐臭成功案例，成功率高达99%，彻底解决狐臭问题！</p>
-						<p style="color: red;font-weight: bold;text-align: center;"><span>每天只通过30个名额</span><br>长按下方二维码添加微信！<br/><span oncopy="myFunction()"><?php echo $stxlwx?></span></p>
+						<p style="color: red;font-weight: bold;text-align: center;"><span>每天只通过30个名额</span><br>长按复制微信号添加老师微信！<br/><span oncopy="myFunction()"><?php echo $stxlwx?></span></p>
 						<!-- <div style="margin-bottom: 20px;">  -->
 						<!-- <img style="text-align: center;max-width: 200px;width: 100%;" class="wechatidpic" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/<span class="weixin_num"><?php echo $stxlwx?></span>.jpg"> -->
 						<!-- </div> -->
-						<p style="text-align: center;">长按复制上方二维码↑↑，添加老师好友！</p>
+						<!-- <p style="text-align: center;">长按复制微信号↑↑，添加老师微信！</p> -->
 					</div>
 					<style>
 						.f-title {
