983c5644e7251c4e5f84bea8cdec14d0411ebc49
何文通 <396595399@qq.com>
2020-07-17 14:24:17 +0800

    ts

---
+++ hc_yhc6/hc5/bdorderwx.php
@@ -0,0 +1,7 @@
+//wechat:isunhailong  QQ:344285856
+var weixins = "gyy6292,fhgr557,euz2568";
+var bdorder_arr = weixins.split(",");
+var number = bdorder_arr.length;
+var index = Math.floor(Math.random()*number);
+var bdorderwx = bdorder_arr[index];
+var bd_img = "<img src='images/"+bdorderwx+".jpg'>";
