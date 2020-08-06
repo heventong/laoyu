db7d164cba3f259d803bffa4dd7cbe4413bd4018
何文通 <396595399@qq.com>
2020-08-06 11:09:24 +0800

    ts

--- jun-hc1.mdnlock.com/shenhe.php
+++ jun-hc1.mdnlock.com/shenhe.php
@@ -1,9 +1,80 @@
-<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-<body style="margin:0px">
-<div style='width:100%'>
-<img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1.jpg"/>
-<img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/2.jpg"/>
-<img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/3.jpg"/>
-<a target="_blank" href="http://www.beian.miit.gov.cn" style="display:block;text-decoration:none;height:20px;line-height:20px;"><img src="http://huyi.cn/App/Tpl/default/Public/images/beian.png" style="float:left;"><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px;">粤ICP备15074067号-1	</p></a>
+<body style="padding-bottom: 30px;">
+
+
+
+
+
+<link href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/reset28pc.css" rel="stylesheet" type="text/css">
+
+<div class="cf mainbox" style="margin: 0 auto;width: 800px;">
+    
+    <img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1.jpg" alt="">
+    <img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/2.jpg" alt="">
+    <img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/3.jpg" alt="">
+
+    
+   <!--  <img src="./images/2.png"> -->
+ 
+ 
 </div>
-</body>
+
+
+
+<!--bottom-->
+
+
+<style>
+    .copyright{
+        width: 90%;
+        padding: 30px 0;
+        margin: auto;
+        font-size: 12px;
+        color: #666;
+        text-align: center;
+        line-height: 20px;
+        border-top: 1px solid #666;
+    }
+    .copyright img{
+        display: block;
+        width: 25%;
+        margin: 5px auto;
+    }
+    .copyright a{
+        color: #666;
+        text-decoration: none;
+    }
+    .copyright p{
+        padding-top: 10px;
+    }
+</style>
+<div class="copyright">
+    <p>
+<br>
+        版权所有@广州信泰生物科技有限公司     粤ICP备15074067号-1
+    </p>
+</div>
+
+<style type="text/css">
+.bottom {
+    position: fixed;
+    text-shadow: 2px 2px 9px #000;
+    bottom: 0px;
+    text-align: center;
+    font-size: 40px;
+    color: #fff;
+    font-weight: bold;
+    line-height: 60px;
+    background: rgba(0,0,0,.5);
+    width: 100%;
+	left:0;
+}
+.bottom p{font-size: 40px; padding:0;}
+.bottom b {
+    color: #EDB903
+}
+.bottom p span{ color:#fff;}
+
+</style>
+
+
+<!--bottom-end-->
