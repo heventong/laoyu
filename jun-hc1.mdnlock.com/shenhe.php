ede2bfccff72aea7f1cb1db4e491e5b2de0de47b
何文通 <396595399@qq.com>
2020-08-06 10:54:07 +0800

    ts

---
+++ jun-hc1.mdnlock.com/shenhe.php
@@ -0,0 +1,9 @@
+<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+<body style="margin:0px">
+<div>
+<img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1.jpg"/>
+<img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/2.jpg"/>
+<img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/3.jpg"/>
+<a target="_blank" href="http://www.beian.miit.gov.cn" style="display:block;text-decoration:none;height:20px;line-height:20px;"><img src="http://huyi.cn/App/Tpl/default/Public/images/beian.png" style="float:left;"><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px;">粤ICP备15074067号-1	</p></a>
+</div>
+</body>
