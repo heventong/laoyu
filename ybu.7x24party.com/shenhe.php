9ca12c57766c9ff756dceec603da7b409eb646b0
何文通 <396595399@qq.com>
2020-08-06 13:51:55 +0800

    add ybu

---
+++ ybu.7x24party.com/shenhe.php
@@ -0,0 +1,111 @@
+<script>
+    //落地页数据收集只需部署一次
+    window._SHDGCONFIG = {id:  '4055', source: 'magneto', data_source: 0};
+    (function (d, s, id) {
+        var n = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        s = d.createElement(s);
+        s.id = id;
+        s.setAttribute('charset', 'utf-8');
+        s.src = '//dmp.sina.com.cn/simpleMonitor.js';
+        n.parentNode.insertBefore(s, n);
+    })(document, 'script', 'monitor-script');
+</script>
+<script>
+    //公共部分代码， 以下代码在整个页面只需要安装一次即可
+    (function (d, s, id) {
+        var n = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        s = d.createElement(s);
+        s.id = id;
+        s.setAttribute('charset', 'utf-8');
+        s.src = '//cnv.ad.sina.com.cn/static/js/fyr.js';
+        n.parentNode.insertBefore(s, n);
+    })(document, 'script', 'fyr-script');
+    _fyr = window._fyr || [];
+    //调用
+    _fyr.push(['_conversion', 'n20988_1_118176', '4055', '1_1', 'ea']);
+</script>
+<body style="padding-bottom: 30px;">
+
+
+
+
+
+<link href="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/reset28pc.css" rel="stylesheet" type="text/css">
+
+<div class="cf mainbox" style="margin: 0 auto;width: 800px;">
+    
+    <img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1.jpg" alt="">
+    <img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/2.jpg" alt="">
+    <img style="width:100%" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/3.jpg" alt="">
+
+    
+   <!--  <img src="./images/2.png"> -->
+ 
+ 
+</div>
+
+
+
+<!--bottom-->
+
+
+<style>
+    .copyright{
+        width: 90%;
+        padding: 30px 0;
+        margin: auto;
+        font-size: 12px;
+        color: #666;
+        text-align: center;
+        line-height: 20px;
+        border-top: 1px solid #666;
+    }
+    .copyright img{
+        display: block;
+        width: 25%;
+        margin: 5px auto;
+    }
+    .copyright a{
+        color: #666;
+        text-decoration: none;
+    }
+    .copyright p{
+        padding-top: 10px;
+    }
+</style>
+<div class="copyright">
+    <p>
+<br>
+        版权所有@广州信泰生物科技有限公司     粤ICP备15074067号-1
+    </p>
+</div>
+
+<style type="text/css">
+.bottom {
+    position: fixed;
+    text-shadow: 2px 2px 9px #000;
+    bottom: 0px;
+    text-align: center;
+    font-size: 40px;
+    color: #fff;
+    font-weight: bold;
+    line-height: 60px;
+    background: rgba(0,0,0,.5);
+    width: 100%;
+	left:0;
+}
+.bottom p{font-size: 40px; padding:0;}
+.bottom b {
+    color: #EDB903
+}
+.bottom p span{ color:#fff;}
+
+</style>
+
+<script>
+    //调用
+    _fyr.push(['_conversion', 'n20988_1_118176', '4055', '1_16', 'ea']);
+</script>
+<!--bottom-end-->
---
+++ ybu.7x24party.com/shenhe6.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!-- saved from url=(0021)http://q8.epport.com/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
+    <title>告别狐臭</title>
+</head>
+
+<body style="width: 725px;max-width: 725px;margin: auto;">
+    <section class="_135editor" data-role="paragraph" data-width="100%" style="margin: 0px auto; width: 100%; flex: 0 0 100%;">
+        <p>
+            <img alt="undefined" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/1.jpg" max-width="790" data-spm-anchor-id="a261y.7663282.descBanner.i0.6d4e71beetfAss" style="border: none; visibility: visible; vertical-align: bottom; max-width: 790px; color: #333333; font-family: &quot;Hiragino Sans GB&quot;, Tahoma, Arial, 宋体, sans-serif; font-size: 12px; text-align: left; background-color: #ffffff; zoom: 1; width: 100%; height: auto;" data-ratio="1.1853333333333333" data-w="750" width="100%"><br style="color: #333333; font-family: &quot;Hiragino Sans GB&quot;, Tahoma, Arial, 宋体, sans-serif; font-size: 12px;       text-align: left;       background-color: #ffffff;  "><img alt="undefined" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/2.jpg" max-width="790" style="border: none; visibility: visible; vertical-align: bottom; max-width: 790px; color: #333333; font-family: &quot;Hiragino Sans GB&quot;, Tahoma, Arial, 宋体, sans-serif; font-size: 12px; text-align: left; background-color: #ffffff; zoom: 1; width: 100%; height: auto;" data-ratio="1.2746666666666666" data-w="750" width="100%"><br style="color: #333333; font-family: &quot;Hiragino Sans GB&quot;, Tahoma, Arial, 宋体, sans-serif; font-size: 12px;       text-align: left;       background-color: #ffffff;  "><img alt="undefined" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/3.jpg" max-width="790" style="border: none; visibility: visible; vertical-align: bottom; max-width: 790px; color: #333333; font-family: &quot;Hiragino Sans GB&quot;, Tahoma, Arial, 宋体, sans-serif; font-size: 12px; text-align: left; background-color: #ffffff; zoom: 1; width: 100%; height: auto;" data-ratio="1.0946666666666667" data-w="750" width="100%"><br style="color: #333333; font-family: &quot;Hiragino Sans GB&quot;, Tahoma, Arial, 宋体, sans-serif; font-size: 12px;       text-align: left;       background-color: #ffffff;  "><img alt="undefined" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/4.jpg" max-width="790" style="border: none; visibility: visible; vertical-align: bottom; max-width: 790px; color: #333333; font-family: &quot;Hiragino Sans GB&quot;, Tahoma, Arial, 宋体, sans-serif; font-size: 12px; text-align: left; background-color: #ffffff; zoom: 1; width: 100%; height: auto;" data-ratio="1.432" data-w="750" width="100%"><br style="color: #333333; font-family: &quot;Hiragino Sans GB&quot;, Tahoma, Arial, 宋体, sans-serif; font-size: 12px;       text-align: left;       background-color: #ffffff;  "><img alt="undefined" src="http://img.heven.top/<?php echo $dirname.'/'.$dirview ?>/5.jpg" max-width="790" style="border: none; visibility: visible; vertical-align: bottom; max-width: 790px; color: #333333; font-family: &quot;Hiragino Sans GB&quot;, Tahoma, Arial, 宋体, sans-serif; font-size: 12px; text-align: left; background-color: #ffffff; zoom: 1; width: 100%; height: auto;" data-ratio="1.7933333333333332" data-w="750" width="100%">
+        </p>
+    </section>
+
+
+</body></html>
