28fdfda1c87b34786761be4460948b93397c6410
何文通 <396595399@qq.com>
2020-08-17 23:00:15 +0800

    ts

---
+++ hc14/hc7/js/getwx.js
@@ -0,0 +1,4 @@
+arr_wx =new Array('kan9685','kaopu2296'); 
+  var wx_index = Math.floor((Math.random() * arr_wx.length)); 
+  var stxlwx = arr_wx[wx_index]; 
+  var img = "<?php echo $pctem['pathurl'] ?>"+arr_wx[wx_index]+".jpg"
